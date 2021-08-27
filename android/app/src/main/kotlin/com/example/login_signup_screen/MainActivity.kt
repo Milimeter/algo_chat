b854341d1c346a27ea70d3731570f7facd328118
Milimeter <razaqolad300@gmail.com>
2021-08-27 09:43:55 -0500

    Cnew push

--- android/app/src/main/kotlin/com/example/login_signup_screen/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.login_signup_screen
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,124 +1,53 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
-import 'package:login_signup_screen/login.dart';
-import 'package:login_signup_screen/signup.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:login_signup_screen/constants/firebase.dart';
+import 'package:login_signup_screen/controllers/algorand_controller.dart';
+import 'package:login_signup_screen/controllers/app_controller.dart';
+import 'package:login_signup_screen/controllers/feeds_controller.dart';
+import 'package:login_signup_screen/controllers/user_controller.dart';
 
-void main() {
-  runApp(MaterialApp(
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initialization.then((value) {
+    print("initialized firebase");
+  });
+  Get.put(UserController());
+
+  Get.put(AppController());
+  Get.put(FeedsController());
+  Get.put(AlgorandController());
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light.copyWith(
+    statusBarBrightness: Brightness.light,
+    statusBarColor: Colors.blue,
+  ));
+
+  runApp(GetMaterialApp(
     debugShowCheckedModeBanner: false,
-    home: HomePage(),
+    home: SplashScreen(),
+    theme: ThemeData(
+      brightness: Brightness.light,
+      fontFamily: "AvenirNext-Medium",
+      visualDensity: VisualDensity.adaptivePlatformDensity,
+      primaryColor: Colors.blue,
+    ),
   ));
 }
 
-class HomePage extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Container(
-          // we will give media query height
-          // double.infinity make it big as my parent allows
-          // while MediaQuery make it big as per the screen
-
-          width: double.infinity,
-          height: MediaQuery.of(context).size.height,
-          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
-          child: Column(
-            // even space distribution
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Column(
-                children: <Widget>[
-                  Text(
-                    "Welcome",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 30,
-
-                    ),
-                    
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text("BackSlash Flutter provides extraordinary flutter tutorials. Do Subscribe! ",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.grey[700],
-                    fontSize: 15,
-
-                  ),)
-                ],
-              ),
-              Container(
-                height: MediaQuery.of(context).size.height / 3,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage("assets/welcome.png")
-                  )
-                ),
-              ),
-
-              Column(
-                children: <Widget>[
-                  // the login button
-                  MaterialButton(
-                    minWidth: double.infinity,
-                    height: 60,
-                    onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage()));
-
-                    },
-                    // defining the shape
-                    shape: RoundedRectangleBorder(
-                      side: BorderSide(
-                        color: Colors.black
-                      ),
-                      borderRadius: BorderRadius.circular(50)
-                    ),
-                    child: Text(
-                      "Login",
-                      style: TextStyle(
-                        fontWeight: FontWeight.w600,
-                        fontSize: 18
-                      ),
-                    ),
-                  ),
-                  // creating the signup button
-                  SizedBox(height:20),
-                  MaterialButton(
-                    minWidth: double.infinity,
-                    height: 60,
-                    onPressed: (){
-                      Navigator.push(context, MaterialPageRoute(builder: (context)=> SignupPage()));
-
-                    },
-                    color: Color(0xff0095FF),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(50)
-                    ),
-                    child: Text(
-                      "Sign up",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w600,
-                        fontSize: 18
-                      ),
-                    ),
-                  )
-
-                ],
-              )
-
-
-
-            ],
-          ),
+      body: Container(
+        child: Center(
+          child: Text("AlgoChat"),
         ),
       ),
     );
   }
 }
-
